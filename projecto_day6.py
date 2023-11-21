58c0c27f882ec745aada891b7c272541eb6c6c85
Gian Severiche <gianseveriche97@gmail.com>
2023-11-21 15:39:09 -0500

    Algunas modificaciones a los nombres para mayor legibilidad

--- projecto_day6.py
+++ projecto_day6.py
@@ -1,6 +1,12 @@
 from pathlib import Path
+
 ruta_acceso = Path('Day_6\Projecto_Day6\Recetas')
 print(f"Bienvenido al recetario\nEsta es la ruta de acceso donde se encuentras nuestras recetas '{ruta_acceso}'")
 
+print("Estas son nuestras recetas disponibles")
+recetas = Path('Day_6\Projecto_Day6\Recetas').glob("**/*.txt")
+for res in recetas:
+    print(res)
+
+
 opciones = list(range(1,7))
-print(opciones)
