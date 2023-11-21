751827746a84aafe454a9d32f946100fdd594704
Gian Severiche <gianseveriche97@gmail.com>
2023-11-21 16:00:05 -0500

    Import Os

--- projecto_day6.py
+++ projecto_day6.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 ruta_acceso = Path('Day_6\Projecto_Day6\Recetas')
 print(f"Bienvenido al recetario\nEsta es la ruta de acceso donde se encuentras nuestras recetas '{ruta_acceso}'")
