eacf0457c2b2ed7535aa15896d650f9050693482
Gian Severiche <gianseveriche97@gmail.com>
2023-11-21 15:18:36 -0500

    Firts commit

---
+++ projecto_day6.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+ruta_acceso = Path('Day_6\Projecto_Day6\Recetas')
+print(f"Bienvenido al recetario\nEsta es la ruta de acceso donde se encuentras nuestras recetas '{ruta_acceso}'")
+
+opciones = list(range(1,7))
+print(opciones)
