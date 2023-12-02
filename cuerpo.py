e344d9143a78750e34bcca1143cb9cd0a79bdc7e
Gian Severiche <gianseveriche97@gmail.com>
2023-12-02 15:33:15 -0500

    Estructura de como de cada opcion y funcion que debe cumplir la aplicación

---
+++ cuerpo.py
@@ -0,0 +1,40 @@
+# Estructura del recetario
+# Mostrar un menu de inicio al usuario
+
+menu = 0
+
+if menu == 1:
+    # Mostrar categorias
+    # Elegir categorias
+    # Mostrar recetas
+    # Elegir receta
+    # Leer receta
+    # Volver a inicio
+    pass
+elif menu == 2:
+    # Mostrar categorias
+    # Elegir categorias
+    # Crear nueva receta
+    # Volver a inicio
+    pass
+elif menu == 3:
+    # Crear categoria
+    # Volver a incio
+    pass
+elif menu == 4:
+    # Mostrar categorias
+    # Elegir categorias
+    # Mostrar recetas
+    # Elegir recetas
+    # Eliminar receta
+    # Volver a inicio
+    pass
+elif menu == 5:
+    # Mostrar categorias
+    # Elegir categorias
+    # Eliminar categoria
+    # Volver a inicio
+    pass
+elif menu == 6:
+    # Terminar el programa
+    pass
