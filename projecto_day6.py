7e9207cabfbe4778792695db34c5a4cba9fe6be7
Gian Severiche <gianseveriche97@gmail.com>
2023-12-02 15:33:57 -0500

    Se hicieron cambios drasticos para mejorar el codigo y sea mas facil de comprender

--- projecto_day6.py
+++ projecto_day6.py
@@ -2,12 +2,33 @@ from pathlib import Path
 import os
 
 ruta_acceso = Path('Day_6\Projecto_Day6\Recetas')
-print(f"Bienvenido al recetario\nEsta es la ruta de acceso donde se encuentras nuestras recetas '{ruta_acceso}'")
 
-print("Estas son nuestras recetas disponibles")
-recetas = Path('Day_6\Projecto_Day6\Recetas').glob("**/*.txt")
-for res in recetas:
-    print(res)
+def contar_recetas(ruta):
+    contador = 0
+    for res in Path(ruta).glob("**/*.txt"):
+        contador += 1
+    return contador
 
+def incio():
+    os.system("cls")
+    print("*" * 50)
+    print("*" * 12 + " Bienvenido al recetario " + "*" * 12)
+    print("*" * 50)
+    print("\n")
+    print(f"Las recetas se encuntran en {ruta_acceso}")
+    print(f"Total de recetas es: {contar_recetas(ruta_acceso)}")
 
-opciones = list(range(1,7))
+    eleccion_menu = "x"
+    while not eleccion_menu.isnumeric() or int(eleccion_menu) not in range(1,7):
+        print("Menu")
+        print('''
+        [1] - Leer receta
+        [2] - Crear nueva receta
+        [3] - Crear categoria nueva
+        [4] - Eliminar receta
+        [5] - Eliminar categoria
+        [6] - Salir de la aplicación''')
+        eleccion_menu = input("Elige una opción: ")
+    return (eleccion_menu)
+
+incio()
