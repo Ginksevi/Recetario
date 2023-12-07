af02ceecef9f22195777323647c95676211bfef7
Gian Severiche <gianseveriche97@gmail.com>
2023-12-07 15:45:06 -0500

    Le realice un cambio de nombre al archivo 'cuerpo.py' ya que hice ese archivo sea el ejecutable del programa y sea más facil de entender

---
+++ programa.py
@@ -0,0 +1,39 @@
+from projecto_day6 import *
+
+while not finalizar_programa:    
+    menu = incio()
+
+    if menu == 1:
+        mis_categorias = mostrar_categoria(ruta_acceso)
+        mi_categoria = elegir_categoria(mis_categorias)
+        mis_recetas = mostrar_recetas(mi_categoria)
+        mi_receta = elegir_recetas(mis_recetas)
+        leer_receta(mi_receta)
+        volver_inicio()
+
+    elif menu == 2:
+        mis_categorias = mostrar_categoria(ruta_acceso)
+        mi_categoria = elegir_categoria(mis_categorias)
+        crear_receta(mi_categoria)
+        volver_inicio()
+
+    elif menu == 3:
+        crear_categoria(ruta_acceso)
+        volver_inicio()
+
+    elif menu == 4:
+        mis_categorias = mostrar_categoria(ruta_acceso)
+        mi_categoria = elegir_categoria(mis_categorias)
+        mis_recetas = mostrar_recetas(mi_categoria)
+        mi_receta = elegir_recetas(mis_recetas)
+        eliminar_receta(mi_receta)
+        volver_inicio()
+
+    elif menu == 5:
+        mis_categorias = mostrar_categoria(ruta_acceso)
+        mi_categoria = elegir_categoria(mis_categorias)
+        eliminar_categoria(mi_categoria)
+        volver_inicio()
+
+    elif menu == 6:
+        finalizar_programa = True
